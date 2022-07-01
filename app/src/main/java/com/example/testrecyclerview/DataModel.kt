1171ee47e039e909d0bebe6561bd0062f77ca819
malyq <maxim99112@gmail.com>
2022-07-01 17:27:24 +0500

    fix fillList

--- app/src/main/java/com/example/testrecyclerview/DataModel.kt
+++ app/src/main/java/com/example/testrecyclerview/DataModel.kt
@@ -1,7 +1,7 @@
 package com.example.testrecyclerview
 
 var randomPosition: Int? = null
-var fillList = mutableListOf(0,1,2,3,4,5)
+var fillList = mutableListOf(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
 
 class DataModel {
     fun addElement() {
